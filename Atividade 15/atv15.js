e3b8b7bb9b741b249225d28348898a151782dbaa
Pedroteodoro25 <78798990+Pedroteodoro25@users.noreply.github.com>
2022-05-29 23:18:05 -0300

    Add files via upload

---
+++ Atividade 15/atv15.js
@@ -0,0 +1,16 @@
+
+const showInfo = () => {
+    const select = document.querySelector('select');
+    const selected = select.options[select.selectedIndex];
+    const url = selected.getAttribute('data-url');
+
+    const confirmation = confirm('Deseja abrir informações do curso?');
+
+    if (confirmation) {
+        newWindow = window.open("", "newWindow", "width=600,height=300");
+        newWindow.document.open();
+        newWindow.document.write(`<iframe src="${url}" style="width: 100%; height: 100%;"></iframe>`);
+        newWindow.document.close();
+        newWindow.focus();
+    }
+};
